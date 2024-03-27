dd9e4d73d53a63f4223696abee47559bd45de2a8
MessiasManoel <161373995+MessiasManoel@users.noreply.github.com>
2024-03-27 10:13:14 -0300

    Add files via upload

--- edit.php
+++ edit.php
@@ -31,3 +31,10 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto PHP Versátil Ti</title>
+    <link rel="stylesheet" type="text/css" href="css/edit.css">
+</head>
+
+<body>
